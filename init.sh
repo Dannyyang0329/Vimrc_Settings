602684db7e6b074a4fd609f4759a3deacfc54f16
Dannyyang0329 <f74094067@gs.ncku.edu.tw>
2022-07-20 12:47:07 +0800

    Fixed bugs

--- init.sh
+++ init.sh
@@ -6,10 +6,3 @@ sudo apt update
 # Download 'git', 'curl', 'gcc', 'vim-gtk3'
 sudo apt install git curl gcc vim-gtk3
 
-# Download vim plugin autoload
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Clone vimrc and bashrc file from repository
-git clone https://github.com/Dannyyang0329/Vimrc_Settings.git
-
-
--- load_vim.sh
+++ load_vim.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
+# Clone vimrc and bashrc file from repository
+git clone https://github.com/Dannyyang0329/Vimrc_Settings.git
+
 # Backup
 mkdir ~/OriginSettings
-cp ./.bashrc ~/OriginSettings
-cp ./.vimrc ~/OriginSettings
-cp -r ./.vim ~/OriginSettings
+cp ~/Vimrc_Settings/.bashrc ~/OriginSettings
+cp ~/Vimrc_Settings/.vimrc ~/OriginSettings
+cp -r ~/Vimrc_Settings/.vim ~/OriginSettings
 touch ~/OriginSettings/README.txt
 echo -e "Two files and one directory is in the current work directory!
  2 Files
-     * .bashrc 
+     * .bashrc
      * .vimrc
  Directory
      * .vim" >> ~/OriginSettings/README.txt
 
 # Adapt settings
-cp ./.bashrc ~/.bashrc
-cp ./.vimrc ~/.vimrc
-cp -r ./vim ~/.vim
+cp ~/Vimrc_Settings/.bashrc ~/
+cp ~/Vimrc_Settings/.vimrc ~/
+cp -r ~/Vimrc_Settings/.vim ~/
 source ~/.bashrc
 
+vim -c PlugInstall
+
 echo "Setting Finish!!!"
+

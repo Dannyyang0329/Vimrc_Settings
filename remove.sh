31d154d8ca740ba3c887ecae9f207a86e077ff81
Dannyyang0329 <f74094067@gs.ncku.edu.tw>
2022-07-23 15:00:16 +0800

    Version 1.1.2 : Fix bugs

--- remove.sh
+++ remove.sh
@@ -3,11 +3,11 @@
 cd ~
 
 if [ -e ~/OriginSettings -a -d ~/OriginSettings ]; then
-    read -p "Do you want to delete directory ~/OriginSettings ? y/n" reply
-    if [ reply = 'y' ]; then
+    read -p "Do you want to delete directory ~/OriginSettings ? (y/n) " reply
+    if [ ${reply} = 'y' ]; then
         rm -rf ~/OriginSettings
         echo "Remove directory : ~/OriginSettings"
-    elif [ reply = 'n' ]; then
+    elif [ ${reply} = 'n' ]; then
         echo "Don't remove directory : ~/OriginSettings"
     else 
         echo "Unknown answer!"
@@ -16,11 +16,11 @@ if [ -e ~/OriginSettings -a -d ~/OriginSettings ]; then
 fi
 
 if [ -e ~/Vimrc_Settings -a -d ~/Vimrc_Settings ]; then
-    read -p "Do you want to delete directory ~/Vimrc_Settings ? y/n" reply
-    if [ reply = 'y' ]; then
+    read -p "Do you want to delete directory ~/Vimrc_Settings ? (y/n) " reply
+    if [ ${reply} = 'y' ]; then
         rm -rf ~/Vimrc_Settings
         echo "Remove directory : ~/Vimrc_Settings"
-    elif [ reply = 'n' ]; then
+    elif [ ${reply} = 'n' ]; then
         echo "Don't remove directory : ~/Vimrc_Settings"
     else 
         echo "Unknown answer!"

fe9c8e560a9503dd4cc9ba30c2ae8e8134bfc994
Dannyyang0329 <f74094067@gs.ncku.edu.tw>
2022-07-31 16:20:14 +0800

    docs: add more packages

--- init.sh
+++ init.sh
@@ -2,11 +2,13 @@
 
 sudo apt update
 
+# Needed for vim setting
 sudo apt install git
 sudo apt install gcc
 sudo apt install vim-gtk3
 sudo apt install powerline
 
+# Other useful packages
 sudo apt install curl
 sudo apt install tree
 sudo apt install python3-pip
@@ -17,3 +19,4 @@ sudo apt install manpages-zh
 sudo apt install finger -y
 sudo apt install mlocate
 
+sudo pip3 install -U Commitizen
